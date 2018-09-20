b6132dd7146ee1a26dd5abeb8fe7cb26f72b05f9
Kazuki <guruguru9606@gmail.com>
2018-09-20 16:11:18 +0900

    Remove comment

--- app.py
+++ app.py
@@ -55,11 +55,3 @@ if __name__ == "__main__":
     port = int(os.getenv('PORT', 8000))
     app.run(host=host, port=port)
 
-    # arg_parser = ArgumentParser(
-    #     usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    # )
-    # arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    # arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    # options = arg_parser.parse_args()
-
-    # app.run(debug=options.debug, port=options.port)
